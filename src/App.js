56ad7e063d8d3210c5955cd6d422ab145491daa0
Kamran <KamranPirishzade@gmail.com>
2024-12-14 23:27:33 -0800

    deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Hour from "./Components/Hour"
+import Timer from "./Components/Timer"
+import Second from "./Components/Second"
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/hour' element={<Hour/>} ></Route>
+          <Route path='/second' element={<Second/>} ></Route>
+          <Route path='/timer' element={<Timer/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
